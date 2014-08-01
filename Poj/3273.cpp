4dce380c8fac36146dc547b79dbe9b8566f0d868
Ronnoc <kybconnor@126.com>
2014-08-01 15:20:32 +0800

    teaching

--- Poj/3273.cpp
+++ Poj/3273.cpp
@@ -1,48 +1,75 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cctype>
 #include <cstdio>
-int x[100010],n,m;
-int cut ( int d ) {
-	int res=1,now=0,i;
-	for ( i=1; i<=n; i++ ) {
-		if ( now+x[i]<=d )
-			now+=x[i];
-		else {
-			now=x[i];
-			res++;
-		}
+#include <string>
+#include <vector>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define PB push_back
+#define MP make_pair
+#define AA first
+#define BB second
+#define OP begin()
+#define ED end()
+#define SZ size()
+#define SORT(x) sort(x.OP,x.ED)
+#define SQ(x) ((x)*(x))
+#define SSP system("pause")
+#define cmin(x,y) x=min(x,y)
+#define cmax(x,y) x=max(x,y)
+typedef long long LL;
+typedef pair<int, int> PII;
+const double eps=1e-8;
+const double PI=acos( -1. );
+const LL MOD = 1000000007;
+int a[100005];
+int N,M;
+int check( int w ) {
+	int ret=0,sum=0;
+	for ( int i=0; i<N; i++ ) {
+		if ( a[i]>w )return N+1;
+		sum+=a[i];
+		if ( sum>w )sum=a[i],ret++;
 	}
-	return res;
-}
-int bfind ( int l,int r ) {
-	while (r>=l) {
-		if ( r-l<=1 ) {
-			if ( cut ( l ) <=m )
-				return l;
-			if ( cut ( r ) <=m )
-				return r;
-			return -1;
-		}
-		int mid= ( l+r ) /2;
-		if ( cut ( mid ) <=m )r=mid;
-		else l=mid+1;
-	}
-	return -1;
+	if ( sum )ret++;
+	return ret;
 }
 int main() {
-//	while ( scanf ( "%d%d",&n,&m ) !=EOF ) {
-		scanf ( "%d%d",&n,&m );
-		int i;
-		for ( i=1; i<=n; i++ )
-			scanf ( "%d",&x[i] );
-		int xmax= 0;
-		for ( i=1; i<=n; i++ )
-			if ( xmax<x[i] )
-				xmax=x[i];
-		int sum=0;
-		for ( i=1; i<=n; i++ )
-			sum+=x[i];
-		int l=xmax,r=sum;
-		int ans=bfind ( l,r );
-		printf ( "%d\n",ans );
-//	}
+	//freopen("","r",stdin);
+	//freopen("","w",stdout);
+	int i,j,k;
+	while ( ~scanf( "%d%d",&N,&M ) ) {
+		for ( i=0; i<N; i++ )scanf( "%d",&a[i] );
+		int l=0,r=0,ans=-1;
+		for ( i=0; i<N; i++ )r+=a[i];
+		for ( i=0; i<N; i++ )cmax( l,a[i] );
+		while ( r>=l ) {
+			if ( r-l<=1 ) {
+				if ( check( l )<=M )ans=l;
+				else if ( check( r )<=M )ans=r;
+				break;
+			}
+			int mid=( r+l )>>1;
+			if ( check( mid )<=M )r=mid;
+			else l=mid;
+		}
+		printf( "%d\n",ans );
+	}
 	return 0;
 }
