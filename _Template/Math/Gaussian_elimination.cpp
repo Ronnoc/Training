4f6a15f7fa2f3ecf78989dd24dca04a197d81aa4
Ronnoc <kybconnor@126.com>
2016-07-26 19:15:02 +0800

    unhappy gauss

--- _Template/Math/Gaussian_elimination.cpp
+++ _Template/Math/Gaussian_elimination.cpp
@@ -45,52 +45,75 @@ int gauss_tpivot(int m, int n) {
 	return 1;
 }
 
-//>验题: poj2345
-
-#define MXN 300
+#define rep(i,a,n) for(int i=(a);i<(int)(n);i++)
+#define MXN 333
 int idx[MXN];
 LL a[MXN][MXN], b[MXN];
-int gaussTpivotMod(int m, int n, int mod = 2) { //mod 下高斯消
-	int i, j, k, row, col;
-	LL maxp, t;
-	int ret = 1;
-	for (i = 0; i < m; i++)idx[i] = i;
-	for (k = 0; k < n; k++) {
-		for (maxp = 0, i = k; i < m; i++)
-			for (j = k; j < n; j++)
-				if (a[i][j] > maxp)
-					maxp = a[row = i][col = j];
-		if (maxp == 0) {
-			bool fail = 0;
-			for (i = k; i < m; i++)if (b[i])fail = 1;
-			if (fail)return 0;
-			ret = k - n;
-			break;
-		}
-		if (col != k)for (swap(idx[col], idx[k]), i = 0; i < m; i++)
-				swap(a[i][col], a[i][k]);
-		if (row != k)for (swap(b[k], b[row]), j = k; j < n; j++)
-				swap(a[k][j], a[row][j]);
-		LL inv = modInv(maxp, mod);
-		for (i = k + 1; i < m; i++) {
-			LL mul = inv * a[i][k];
-			for (j = k; j < n; j++)
-				a[i][j] -= a[k][j] * mul;
-			b[i] -= mul * b[k];
-		}
-		for (i = k; i < m; i++)
-			for (j = k; j < n; j++) {
-				LL &tmp = a[i][j];
-				if (tmp >= mod)tmp %= mod;
-				if (tmp < 0)tmp = tmp % mod + mod;
-			}
-	}
-	for (i = n - 1; i >= 0; i--)
-		for (j = i + 1; j < n; j++)
-			b[i] -= a[i][j] * b[j];
-	for (k = 0; k < n; k++)
-		a[0][idx[k]] = b[k];
-	for (k = 0; k < n; k++)
-		b[k] = (a[0][k] % mod + mod) % mod;
-	return ret;
+LL extGcd (LL a, LL b, LL &x, LL &y) {
+    LL ret = a;
+    if (b) {
+        ret = extGcd (b, a % b, y, x);
+        y -= (a / b) * x;
+    } else x = 1, y = 0;
+    return ret;
+}
+LL modInv (LL a, LL m) {
+    LL x, y;
+    extGcd(a, m, x, y);
+    return (m + x % m) % m;
+}
+
+void fit(LL &a,LL mod){a=(a%mod+mod)%mod;}
+pair<LL,LL>singleMod(LL A,LL B,LL M){
+	LL d = __gcd( M, A );
+	if ( B % d )return MP( 0, -1 );
+	LL m = M / d;
+	LL t = B / d * modInv( A / d, m ) % m;
+	return MP( ( ( t % m ) + m ) % m, m );
+}
+int gaussTpivotMod(int m, int n, LL mod) { //mod 下高斯消
+    int ret = 0,fail = 0;
+    rep(i,0,m){
+        idx[i]=i;
+        fit(b[i],mod);
+        rep(j,0,n)fit(a[i][j],mod);
+    }
+    rep(k,0,n){
+        LL maxp=0;int row,col;
+        rep(i,k,m)rep(j,k,n)if(maxp==0)
+            maxp=a[row=i][col=j];
+        if(maxp==0){
+            rep(i,k,m)fail|=b[i]!=0;
+            if(fail)return -1;
+            ret=n-k;
+            break;
+        }
+        swap(idx[col],idx[k]);
+        rep(i,0,m)swap(a[i][col],a[i][k]);
+        swap(b[k], b[row]);
+        rep(j,k,n)swap(a[k][j], a[row][j]);
+        rep(i,k+1,m){
+            while(a[i][k]){
+                if(a[i][k]<a[k][k]){
+                    swap(b[k],b[i]);
+                    rep(j,0,n)swap(a[k][j], a[i][j]);
+                }
+                LL cnt=a[i][k]/a[k][k];
+                rep(j,0,n)a[i][j]-=cnt*a[k][j];
+                b[i]-=cnt*b[k];
+                rep(j,0,n)fit(a[i][j],mod);
+                fit(b[i],mod);
+            }
+        }
+    } 
+    for (int i = n - 1; i >= 0; i--){
+    	rep(j,i+1,n)
+        	b[i] -= a[i][j] * b[j];
+        if(a[i][i])
+        	b[i]=singleMod(a[i][i],b[i],mod).AA;
+    }
+    rep(k,0,n)a[0][idx[k]] = b[k];
+    rep(k,0,n)b[k]=a[0][k];
+    rep(k,0,n)fit(b[k],mod);
+    return ret;
 }
