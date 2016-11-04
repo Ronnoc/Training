f868ec72ba02fb6d336c3e1a74738f054c157e11
Yanbin Kang <kybconnor@126.com>
2016-11-04 15:19:17 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,7 @@ curl -fLo ~/.vim/autoload/plug.vim \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # my .vimrc 
-curl -fLo ~/.vimrc \
-      https://raw.githubusercontent.com/Ronnoc/Training/master/.vimrc
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/Ronnoc/Training/master/.vimrc
 
 # my .zshrc
-curl -fLo ~/.zshrc \
-      https://raw.githubusercontent.com/Ronnoc/Training/master/.zshrc
+curl -fLo ~/.zshrc https://raw.githubusercontent.com/Ronnoc/Training/master/.zshrc
