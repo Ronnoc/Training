88bec5fc4356019797ecfde5c0d8a1c84956e6ea
Yanbin Kang <kybconnor@126.com>
2016-11-21 11:40:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
 # install vim 
-mkdir -p ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# my .vimrc 
-curl -fLo ~/.vimrc https://raw.githubusercontent.com/Ronnoc/Training/master/.vimrc
 
 # my .zshrc
 curl -fLo ~/.zshrc https://raw.githubusercontent.com/Ronnoc/Training/master/.zshrc
